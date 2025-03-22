268c2da7ef8120b3a41b893e26c7ca252ca4d1c1
FriskIsGit <friskisgit@users.noreply.github.com>
2025-03-22 16:39:50 +0100

    Fix extension

--- content.js
+++ content.js
@@ -1,11 +1,2 @@
 
-function logInfo(...args) {
-    const message = args.join(' ');
-    console.info("%c[Pocketify]", "color: green;", message);
-}
-
-function logWarn(...args) {
-    const message = args.join(' ');
-    console.warn("%c[Pocketify]", "color: red;", message);
-}
 
--- pocketify.js
+++ pocketify.js
@@ -16,4 +16,14 @@ function hideIfShown(element) {
 function main() {
 }
 
+function logInfo(...args) {
+    const message = args.join(' ');
+    console.info("%c[Pocketify]", "color: green;", message);
+}
+
+function logWarn(...args) {
+    const message = args.join(' ');
+    console.warn("%c[Pocketify]", "color: red;", message);
+}
+
 main();
